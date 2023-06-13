3260fab3676955957ce2a4540bd8a478e4c1e7f1
maksym <maksym.nevinchanyi@studytafensw.edu.au>
2023-06-13 10:48:04 +1000

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    window.location.href = "about us.html";
+  });
+document.getElementById("q").addEventListener("click", function() {
+    window.location.href="contact us.html"
+})
